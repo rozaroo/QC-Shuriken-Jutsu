2b4735a45b16bc34bf83387b207a17c406fe0570
rozaroo <roramos@uade.edu.ar>
2026-05-10 19:47:19 -0300

    Feedback visual de los botones de shuriken

--- Assets/Scripts/ShurikenSelector.cs
+++ Assets/Scripts/ShurikenSelector.cs
@@ -1,19 +1,19 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ShurikenSelector : MonoBehaviour
 {
     public MainMenuManager menu;
+    public Image[] shurikenButtonImages;
     private int ShurikenOneCount = 0;
     private int ShurikenTwoCount = 0;
     private int ShurikenThreeCount = 0;
-    private void Start()
-    {
-        menu = FindObjectOfType<MainMenuManager>();
-    }
+    
     public void SelectShuriken(int index) 
     {
         if (ShurikenData.Instance != null) ShurikenData.Instance.selectedShurikenIndex = index;
+        StartCoroutine(ButtonFlash(shurikenButtonImages[index]));
         //Enviar evento a Analytics
         string userId = menu.userId;
         if (ShurikenData.Instance.selectedShurikenIndex == 0) 
@@ -34,6 +34,13 @@ public class ShurikenSelector : MonoBehaviour
             Debug.Log($"[ShurikenSelector] Usuario: {userId} seleccionó el shuriken #{index}, {ShurikenThreeCount} veces");
             AnalyticsManager.Instance.ShurikenSelected(index, userId, ShurikenThreeCount);
         }
+    }
+    IEnumerator ButtonFlash(Image buttonImage)
+    {
+        Color originalColor = buttonImage.color;
+        buttonImage.color = Color.gray;
+        yield return new WaitForSeconds(0.15f);
+        buttonImage.color = originalColor;
     }
 }
 
